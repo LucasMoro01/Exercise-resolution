c6b5d530259a024ea16f01d004b119a0bb84f5f0
LucasMoro01 <lucasmoro_07@outlook.com>
2019-08-11 02:26:10 -0300

    1010 e 1003

---
+++ uri/calculoSimples1010.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class calculoSimples1010 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a, b, d, e;
+        float c, f, soma;
+
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextFloat();
+        d = scan.nextInt();
+        e = scan.nextInt();
+        f = scan.nextFloat();
+
+        soma = (b * c) + (e * f);
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", soma);
+        scan.close();
+    }
+}
---
+++ uri/somaSimples1003.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class somaSimples1003 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a, b, soma = 0;
+        a = scan.nextInt();
+        b = scan.nextInt();
+
+        soma = a + b;
+        System.out.println("SOMA = " + soma);
+        scan.close();
+    }
+}
